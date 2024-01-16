fc78e91bb835049a4fc34762ca6e05ceab8f2b2a
Maclean Levin <maclean@forge.eco>
2024-01-15 19:16:39 -0500

    config

--- FirebaseConfig.tsx
+++ FirebaseConfig.tsx
@@ -3,14 +3,14 @@ import {initializeApp} from "firebase/app";
 import {getAuth, initializeAuth, getReactNativePersistence} from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyDl9fNdPTySNrhAWcdAP4-hu2TGJ48ysk4",
-  authDomain: "pudotest-ba3dd.firebaseapp.com",
-  databaseURL: "https://pudotest-ba3dd-default-rtdb.firebaseio.com",
-  projectId: "pudotest-ba3dd",
-  storageBucket: "pudotest-ba3dd.appspot.com",
-  messagingSenderId: "922269500321",
-  appId: "1:922269500321:web:c527db7914e42a325b01e0",
-  measurementId: "G-BYTWCS56K3",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
